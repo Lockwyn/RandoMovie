a68a37c8e5fe0e8ff0dfdfc34c40e690dd9d516b
Lockwyn <lockwyn@gmail.com>
2021-07-29 17:30:07 -0500

    fix #3 fix goodmovie to wattch fetch from pagination

--- goodmovietowatch.py
+++ goodmovietowatch.py
@@ -1,9 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
-import random
-urls=[
-    "https://agoodmovietowatch.com/netflix/23-best-netflix/"
-]
+
+
+
+def process():
+    movies=[]
+    urls = [
+        "https://agoodmovietowatch.com/netflix/23-best-netflix/",
+
+    ]
+    for url in urls:
+        movies=movies+scrapUrl_Gmtw(url)
+    return movies
+
+def getUrls():
+    n_pages = 10
+    base_link = "https://agoodmovietowatch.com/netflix/23-best-netflix/"
+    urls = []
+    for i in range(1,n_pages+1):
+        url=base_link
+        if i>1:
+            url=base_link+f"page/{i}/"
+        urls.append(url)
+    return urls
+
+
 def scrapUrl_Gmtw(url):
     movies = []
     page = requests.get(url)
@@ -11,15 +32,16 @@ def scrapUrl_Gmtw(url):
     h2=soup.find_all("h2")
     for h in h2:
         entry=h.find("a").text.split("(")
-        movies.append({'title': entry[0].strip(),'year': entry[1]})
+        if len(entry)>1:
+            movies.append({'title': entry[0].strip(),'year': entry[1]})
+        else:
+            movies.append( {'title': entry[0].strip(), 'year': "NA"} )
     return movies
 
 
 def process():
     movies=[]
-    urls = [
-        "https://agoodmovietowatch.com/netflix/23-best-netflix/"
-    ]
+    urls = getUrls()
     for url in urls:
         movies=movies+scrapUrl_Gmtw(url)
     return movies
